a4b60f92f3451b6997a85beeedbf41d4bd22bcd1
seedpcseed <seedpcseed@gmail.com>
2021-01-16 08:48:12 -0600

    update db find in rRNAFinder.pl

--- bin/rRNAFinder.pl
+++ bin/rRNAFinder.pl
@@ -27,7 +27,7 @@ setOptions();
 my $bin = "$FindBin::RealBin";
 $DBDIR ||= "$FindBin::RealBin/../db";
 
-my $rna_hmmdir = "$bin/../db/hmm/rna";
+my $rna_hmmdir = "$DBDIR/hmm/rna";
 if (! -e $outdir) {
     msg("Creating new output folder: $outdir");
     my $cmd = "mkdir -p \Q$outdir\E";
@@ -62,18 +62,18 @@ else{
 foreach my $kdom (@domains){
 
     my $hmmdb = "$rna_hmmdir/$kdom.hmm";
-    
+
     $hmmdb && -r $hmmdb or err("Can't find database: $hmmdb");
     # run the external command
     my $search_output = "$outdir/$kdom.tblout";
     my $cmd = "nhmmer --cpu $threads -E $evalue -o /dev/null --tblout $search_output \Q$hmmdb\E \Q$fasta\E";
-    
+
     msg("Scanning $fasta for $kdom rRNA genes using $hmmdb... please wait");
     msg("Command: $cmd");
     if(! -e $search_output){
 	system($cmd) >> 8 and  die "Could not execute cmd=$cmd, $!\n";
     }
-        
+
     # target name 0, accession 1, query name 2, accession 3, hmmfrom 4, hmm to 5, alifrom 6, ali to 7, envfrom 8, env to 9, sq len 10, strand 11, E-value 12, score 13, bias 14, description of target 15
 
     #for each location, only the best hit (lowest evalue and highest score) is reported
@@ -82,16 +82,16 @@ foreach my $kdom (@domains){
 	chomp;
 	next if /^#/;    # comment line
 	next if !/\S/; #empty line
-	
+
 	my @line = split(/\s+/, $_);
 	err("bad line in nhmmer output - @line") unless defined $line[6] and $line[6] =~ m/^\d+$/;
 	my($alifrom,$alito,$strand) = $line[6] < $line[7] ? ($line[6],$line[7],'+') : ($line[7],$line[6],'-');
 	my($seqid, $qname, $evalue, $score) = ($line[0], $line[2], $line[12], $line[13]);
-	
+
 	next if($qname !~ /5S|5_8S/ && $score < 180);
-			
+
 	my $loc= $alifrom < $alito ? "$alifrom:$alito": "$alito:$alifrom";
-	
+
 	if(exists $read2hmmhit{$seqid}->{$loc}){
 	    if($read2hmmhit{$seqid}->{$loc}->{evalue} > $evalue || $read2hmmhit{$seqid}->{$loc}->{score} < $score){
 		$read2hmmhit{$seqid}->{$loc}->{kdom} = $kdom;
@@ -128,12 +128,12 @@ foreach my $seqid (keys %read2hmmhit){
     foreach my $loc1 (keys %{$read2hmmhit{$seqid}}){
 	foreach my $loc2 (keys %{$read2hmmhit{$seqid}}){
 	    next if $loc1 eq $loc2;
-	    my ($s1, $e1) = split(/:/, $loc1);    
+	    my ($s1, $e1) = split(/:/, $loc1);
 	    my($s2, $e2) = split(/:/, $loc2);
-	    
+
 	    if(is_overlapping($s1, $e1, $s2, $e2)){
-		
-		if($read2hmmhit{$seqid}->{$loc1}->{evalue} > $read2hmmhit{$seqid}->{$loc2}->{evalue} 
+
+		if($read2hmmhit{$seqid}->{$loc1}->{evalue} > $read2hmmhit{$seqid}->{$loc2}->{evalue}
 		   || $read2hmmhit{$seqid}->{$loc1}->{score} < $read2hmmhit{$seqid}->{$loc2}->{score}){
 		    push(@dels, $loc1);
 		}
@@ -165,9 +165,9 @@ foreach my $seqid (keys %read2hmmhit){
 	my $strand = $read2hmmhit{$seqid}->{$loc}->{strand};
 	my $evalue = $read2hmmhit{$seqid}->{$loc}->{evalue};
 	my $score = $read2hmmhit{$seqid}->{$loc}->{score};
-	
+
 	my $fragLen =  $alito-$alifrom;
-	
+
 	if($qname =~ m/(16S|18S|23S|28S)/ && $fragLen < $len){
 	    msg("Skipping short rRNA: len=$fragLen $seqid\t$kdom\t$qname\t$alifrom\t$alito\t$evalue\t$strand\t$score\n");
 	    next;
@@ -216,7 +216,7 @@ sub is_overlapping{
 
 sub rna2taxon{
     my ($rnatcats) = @_;
-    
+
     my %taxfiles = ();
     foreach my $cat (sort keys %$rnatcats){
 	my $input = "$cat.ffn";
@@ -236,7 +236,7 @@ sub rna2taxon{
 	my $cmd = "$^X $bin/rna2taxon.pl --dbdir $DBDIR/blast --cpus $threads --dbtype $dbtype --evalue $evalue --identities $identities --coverage $coverage $outdir/$input > $outdir/$output";
 	msg("Command: $cmd");
 	system($cmd) >> 8 and  die "Could not execute cmd=$cmd, $!\n";
-	
+
     }
     if(-e "$outdir/rRNA.tax.txt"){
 	unlink("$outdir/rRNA.tax.txt");
@@ -244,15 +244,15 @@ sub rna2taxon{
     foreach (keys %taxfiles){
 	print STDERR "$_\n";
 	my $catcmd = "cat $taxfiles{$_} >> $outdir/rRNA.tax.txt";
-	
+
 	msg("####################Command: $catcmd");
 	system($catcmd) >> 8 and  die "Could not execute cmd=$catcmd, $!\n";
     }
-    
+
 }
 sub gff2fasta{
     my ($gff, $fasta, $rnacats) = @_;
-    
+
     my %rnas = ();
     open(GFF, $gff) or die "Could not open $gff to read, $!\n";
     open(FASTA, $fasta) or die "Could not open $fasta to read, $!\n";
@@ -260,7 +260,7 @@ sub gff2fasta{
     foreach (<GFF>){
 	next if /^##/;
 	my @a = split(/\s+/, $_);
-	
+
        #9735487 rRNAFinder.pl   bac_16SrRNA        2       66      3.3e-12 -       .       ID=9735487_1;Name=bac_16SrRNA
 	my $rnatype = $a[2];
 	my ($id) =  $a[8] =~ /ID=(\S+?);/;
@@ -336,16 +336,16 @@ sub setOptions {
       {OPT=>"version", VAR=>\&version,           DESC=>"Print version and exit"},
       {OPT=>"quiet!",  VAR=>\$quiet, DEFAULT=>0, DESC=>"No screen output"},
       {OPT=>"dbdir=s",  VAR=>\$DBDIR, DEFAULT=>"", DESC=>"metaerg searching database directory"},
-      
+
       {OPT=>"outdir=s",  VAR=>\$outdir, DEFAULT=>'.', DESC=>"Output folder [.]"},
       {OPT=>"threads=i",  VAR=>\$threads, DEFAULT=>8,  DESC=>"Number of threads/cores/CPUs to use"},
-      
+
       {OPT=>"evalue=f",VAR=>\$evalue, DEFAULT=>1E-09, DESC=>"evalue cut-off for gene prediction and taxon assignment"},
       {OPT=>"domain=s",VAR=>\$domain, DEFAULT=>'meta', DESC=>"Search rRNA for rRNA in domains: [arc|bac|euk|meta]"},
-      
+
       'rRNA gene prediction options',
       {OPT=>"length=s",  VAR=>\$len, DEFAULT=>180, DESC=>"length cut-off for 16/18/23/28S rRNA only"},
-      
+
       'rRNA gene taxon assignment options:',
       {OPT=>"coverage=f",VAR=>\$coverage, DEFAULT=>80, DESC=>"The min percent of the query sequence ovelaping with the subject sequence"},
       {OPT=>"identities=s",VAR=>\$identities, DEFAULT=>'70,80,85,90,95,97,99', DESC=>"the percent identity of a match must exceed the given value of percent identity to be assigned at the given rank:domain70, phylum 80, class 85, order 90, family 95, genus 97, species 99"}
